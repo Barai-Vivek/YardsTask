718fe9325474c640b88fec995cbdad1a3cd652e1
vivekbarai <barai.vivek92@gmail.com>
2024-01-27 17:38:42 +0530

    Added persist storage

--- App.tsx
+++ App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import HomeScreen from './src/HomeScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
-import {store} from './src/redux/store';
+import {persistor, store} from './src/redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <HomeScreen />
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <HomeScreen />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,28 @@
 import {configureStore} from '@reduxjs/toolkit';
 import rootReducer from './reducer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer, persistStore} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+const persistor = persistStore(store);
+
+// const store = configureStore({
+//   reducer: rootReducer,
+// });
 
 export const setupStore = () => {
   return configureStore({
@@ -13,4 +32,4 @@ export const setupStore = () => {
 
 export type AppStore = ReturnType<typeof setupStore>;
 
-export {store};
+export {store, persistor};
